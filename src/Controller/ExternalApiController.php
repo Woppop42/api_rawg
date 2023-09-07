821e2873c5d6bf4bb6a7fe8c102f884840ef2717
Gabriel <gokba27@gmail.com>
2023-09-07 15:05:38 +0200

    algo de recup de jeux et de genres via rawg

--- src/Controller/ExternalApiController.php
+++ src/Controller/ExternalApiController.php
@@ -2,6 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\Games;
+use App\Entity\Genres;
+use App\Repository\GenresRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -11,13 +15,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class ExternalApiController extends AbstractController
 {
     #[Route('/api/external/getSfDoc', name: 'external_api', methods: 'GET')]
-    public function getSfDoc(HttpClientInterface $httpClient): Response
+    public function getSfDoc(HttpClientInterface $httpClient, EntityManagerInterface $manager, GenresRepository $repo): Response
     {
         try {
             $apiKey = 'ee18b08be15945d6b49d93eaa117d2a9';
             $response = $httpClient->request(
                 'GET',
-                'https://api.rawg.io/api/genres?key=' . $apiKey, 
+                'https://api.rawg.io/api/games?page=2&page_size=100&key=' . $apiKey, 
                 [
                     'headers' =>[
                         'Authorization' => 'Bearer' . $apiKey,
@@ -30,6 +34,33 @@ class ExternalApiController extends AbstractController
             if ($response->getStatusCode() === 200) {
                 // Si la réponse est OK, renvoyez les données JSON
                 $json = json_decode($response->getContent(), true);
+                foreach ($json['results'] as $gameData)
+                {
+                    $game = new Games;
+                    $game->setName($gameData['name']);
+                    $game->setDateSortie($gameData['released']);
+                    $game->setImage($gameData['background_image']);
+                    if(isset($gameData['genres']) && is_array($gameData['genres'])) {
+                        foreach ($gameData['genres'] as $genreData)
+                        {
+                            $genreName = $genreData['name'];
+            
+                            // Vous pouvez récupérer l'entité Genres à partir de la base de données par son nom
+                            $genre = $repo->findOneBy(['name' => $genreName]);
+            
+                            // Si l'entité Genres n'existe pas encore, vous pouvez la créer
+                            if (!$genre) {
+                                $genre = new Genres();
+                                $genre->setName($genreName);
+                            }
+            
+                            $game->addGenre($genre);
+                        }
+                    }
+                    $manager->persist($game);
+                    
+                }
+                $manager->flush();
                 return $this->render('external_api/index.html.twig', [
                     'json' => $json,
                 ]);
---
+++ src/Entity/Games.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\GamesRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: GamesRepository::class)]
+class Games
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $date_sortie = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $image = null;
+
+    #[ORM\ManyToMany(targetEntity: Genres::class, inversedBy: 'games')]
+    private Collection $genres;
+
+    public function __construct()
+    {
+        $this->genres = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getDateSortie(): ?string
+    {
+        return $this->date_sortie;
+    }
+
+    public function setDateSortie(string $date_sortie): static
+    {
+        $this->date_sortie = $date_sortie;
+
+        return $this;
+    }
+
+    public function getImage(): ?string
+    {
+        return $this->image;
+    }
+
+    public function setImage(string $image): static
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Genres>
+     */
+    public function getGenres(): Collection
+    {
+        return $this->genres;
+    }
+
+    public function addGenre(Genres $genre): static
+    {
+        if (!$this->genres->contains($genre)) {
+            $this->genres->add($genre);
+        }
+
+        return $this;
+    }
+
+    public function removeGenre(Genres $genre): static
+    {
+        $this->genres->removeElement($genre);
+
+        return $this;
+    }
+}
